e7e8454bf6e5b300eec78d639d5608bcbe8cbf64
Gabriel Moral <gabrielmoralortiz@gmail.com>
2016-04-30 13:42:10 +0200

    Remove json parsing

--- render.js
+++ render.js
@@ -1,9 +1,7 @@
 function load() {
 
-    $.get('http://gabrielmoral.github.io/Resume/data.json', function (data) {
+    $.get('http://gabrielmoral.github.io/Resume/data.json', function (resumeData) {
         $.get('http://gabrielmoral.github.io/Resume/resumeTemplate.html', function (resumeTemplate) {
-
-            var resumeData = JSON.parse(data);
 
             var rendered = Mustache.render(resumeTemplate, resumeData);
             $('#target').html(rendered);
